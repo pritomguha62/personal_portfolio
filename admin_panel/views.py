2a446580ccc1a95d70301a2e4230cd791b8b22c7
pritomguha62 <pritomguha62@gmail.com>
2024-12-13 02:35:55 +0600

    admin_panel update..

--- admin_panel/views.py
+++ admin_panel/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .forms import UserRegistrationForm
+from django.contrib.auth import login
 
 from .models import *
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+
+# User = get_user_model()
 
 # Create your views here.
 
@@ -23,31 +27,18 @@ def signin_info(request):
 
         password = request.POST.get('password')
 
-        return HttpResponse(request.POST.get('email') + request.POST.get('password'))
+        # return HttpResponse(request.POST.get('email') + request.POST.get('password'))
 
 def signup_info(request):
 
-    if request.method == "POST":
-
-        if request.POST.get('terms_privacy') != 'on':
-
-            previous_url = request.META.get('HTTP_REFERER', '/')
-
-            return redirect(previous_url)
-
-        name = request.POST.get('name')
-
-        email = request.POST.get('email')
-
-        password = request.POST.get('password')
-
-        User.objects.create(
-            name = name,
-            email = name,
-        )
-
-        User.set_password(password)
-        User.save()
-
-        return HttpResponse(request.POST.get('email') + request.POST.get('password'))
-
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+            login(request, user)  # Log the user in after registration
+            return redirect('home')  # Replace 'home' with your desired redirect URL
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'admin_templates/admin_signup.html', {'form': form})
